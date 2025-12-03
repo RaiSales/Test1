fb5a1c181ee3e03f0c0cadac6c86c964d93b715e
R10 <118781798+RaiSales@users.noreply.github.com>
2025-12-03 00:23:06 -0300

    Delete ProjetoGoat directory

--- ProjetoGoat/paginas/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import P1
-
-urlpatterns = [
-        path('',P1.as_view(), name='/p1')
-    ]
--- ProjetoGoat/paginas/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-class P1(TemplateView):
-    template_name = "paginas/p1.html"
-# Create your views here.
